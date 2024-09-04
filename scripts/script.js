861a964114d4c43068ce59f19a0e4889ef841a1a
Raheel Ahmed <101556343+raheel-436@users.noreply.github.com>
2024-09-04 16:51:19 +0500

    'Added FAQs Accordian'

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,19 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0); //toggle this class on 0 pixel scroll at Y axis
 });
+
+//show or hide faq
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+    //for changing icon
+    const faqIcon = faq.querySelector(".faq__icon i");
+    if (faqIcon.className === "fa-solid fa-plus") {
+      faqIcon.className = "fa-solid fa-minus";
+    } else {
+      faqIcon.className = "fa-solid fa-plus";
+    }
+  });
+});
